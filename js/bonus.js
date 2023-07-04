5854fd3fd1406725af73d47629427466582803a0
NaP <annposillipo@gmail.com>
2023-07-04 02:07:19 +0200

    not_sure

--- js/bonus.js
+++ js/bonus.js
@@ -1,3 +1,6 @@
+
+
+
 function submitForm(event){
     event.preventDefault(); 
     let kmToGo = parseInt(document.getElementById("inputKm").value);
@@ -11,13 +14,31 @@ else if (age > 65){
   price = price - (40 * price /100);}
 
   document.getElementById("price").textContent = "Il prezzo è " + price;
+
+
+
+ 
 }
 
- function submitFormS(event){
-    event.preventDefault(); 
-    let name = document.getElementById("inputNome");
-    let cognome = document.getElementById("inputCognome");
+
+document.getElementById("myFormS").addEventListener("submit", submitFormS);
+
+
+
+function submitFormS(event) {
+    event.preventDefault();
     
+    let name = document.getElementById("inputNome").value;
+    let cognome = document.getElementById("inputCognome").value;
+    let km = document.getElementById("inputKm").value;
+    let price = document.getElementById("price").value;
+    
+    let ticket = "Nome: " + name + "\nCognome: " + cognome + "\nKm da percorrere: " + km + "\nPrezzo:" + price
+
+    document.getElementById("ticket_2").textContent =  ticket;
+}
 
 
- }
+ 
+
+ 
